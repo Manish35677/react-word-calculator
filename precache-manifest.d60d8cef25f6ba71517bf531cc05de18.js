44b83031dccb402a0cfdc9c86ab666af92a22507
manish <mpmanishkumar3@gmail.com>
2022-09-30 12:01:30 +0530

    Updates

---
+++ precache-manifest.d60d8cef25f6ba71517bf531cc05de18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f22cc568b265e6c5f3",
+    "url": "/static/css/main.25e97ed1.chunk.css"
+  },
+  {
+    "revision": "32f22cc568b265e6c5f3",
+    "url": "/static/js/main.32f22cc5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "19fe59ef5311eecd93c3f18ea9dad9b6",
+    "url": "/index.html"
+  }
+];
