65bf636e855d59bc96dfcfe828f29737beef1449
manish <mpmanishkumar3@gmail.com>
2022-09-30 13:18:37 +0530

    Updates

---
+++ precache-manifest.8a7e1269ff4e0ccdca9df5c74298e6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f22cc568b265e6c5f3",
+    "url": "/react-word-calculator/static/css/main.25e97ed1.chunk.css"
+  },
+  {
+    "revision": "32f22cc568b265e6c5f3",
+    "url": "/react-word-calculator/static/js/main.32f22cc5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-word-calculator/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/react-word-calculator/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "191172db46908a51a126",
+    "url": "/react-word-calculator/static/js/runtime~main.191172db.js"
+  },
+  {
+    "revision": "edb20432f8341937658c2350f16652e4",
+    "url": "/react-word-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d60d8cef25f6ba71517bf531cc05de18.js"
+  "/react-word-calculator/precache-manifest.8a7e1269ff4e0ccdca9df5c74298e6c5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-word-calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
